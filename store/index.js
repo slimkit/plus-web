e798d587a35e7097aec5521be2ea365479b8b024
mutoe <mutoe@foxmail.com>
2019-01-04 16:12:34 +0800

    chore: 加载 bootstrappers 配置

---
+++ store/index.js
@@ -0,0 +1,28 @@
+import { cookie } from '@/utils/storage'
+
+export const state = () => ({
+  boot: {},
+})
+
+export const TYPES = {
+  SAVE_BOOT: 'SAVE_BOOT',
+}
+
+export const mutations = {
+  [TYPES.SAVE_BOOT] (state, boot) {
+    state.boot = boot
+  },
+}
+
+export const actions = {
+  async nuxtServerInit ({ commit, dispatch }, { req }) {
+    const token = cookie.get('access_token', req.headers.cookie)
+    commit('auth/SAVE_TOKEN', token)
+    await dispatch('getBootstrappers')
+  },
+
+  async getBootstrappers ({ commit }) {
+    const boot = await this.$axios.$get('/bootstrappers')
+    commit(TYPES.SAVE_BOOT, boot)
+  },
+}
