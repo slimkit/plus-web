f326becc9f194d344d0d0ce019ce15fd2d8d5a4d
mutoe <mutoe@foxmail.com>
2019-01-23 17:52:46 +0800

    feat: 申请认证

--- pages/setting/certificate/_edit.vue
+++ pages/setting/certificate/_edit.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="p-setting-certificate-edit">
     <nav class="nav-wrap">
-      <span>{{ typeMap[cert.certification_name] || '个人认证' }}</span>
-      <!-- TODO: 机构认证 -->
+      <template v-if="cert.certification_name">
+        <span>{{ typeMap[cert.certification_name] || '个人认证' }}</span>
+      </template>
+      <template v-else>
+        <nuxt-link :to="{query: {type: 'user'}}" :class="{'exact-active': type === 'user'}">个人认证</nuxt-link>
+        <nuxt-link :to="{query: {type: 'org'}}" :class="{'exact-active': type === 'org'}">机构认证</nuxt-link>
+      </template>
     </nav>
 
     <div class="content-wrap">
       <IForm
         ref="form"
-        v-model="form"
-        :label-width="80"
+        :model="form"
+        :label-width="100"
         :rules="rules"
         @submit.native.prevent="onSubmit"
       >
@@ -19,7 +24,7 @@
           :prop="item"
           :label="labels[item].label"
         >
-          <IInput v-model="form[item]" />
+          <IInput v-model="form[item]" :maxlength="item === 'phone' ? 11 : null" />
         </IFormItem>
 
         <IFormItem
@@ -27,16 +32,27 @@
           prop="file"
           label="认证资料"
         >
-          <AsyncFile
+          <figure
             v-for="(id, index) in form.files"
+            v-if="!isOrg || index === 0"
             :key="`${index}-${id}`"
             class="upload-placeholder"
-            type="image-wrap"
-            :file="{id}"
-            :max-width="232"
-            :max-height="163"
-            @click.native="$refs[`uploader${index}`].select()"
-          />
+          >
+            <AsyncFile
+              type="image-wrap"
+              :file="{id}"
+              :max-width="232"
+              :max-height="163"
+              @click.native="$refs[`uploader${index}`].select()"
+            />
+
+            <figcaption>
+              <svg class="icon xl"><use xlink:href="#icon-ico_upload" /></svg>
+              <template v-if="isOrg">上传机构营业执照</template>
+              <template v-else-if="index === 0">上传身份证正面照片</template>
+              <template v-else>上传身份证反面照片</template>
+            </figcaption>
+          </figure>
 
           <Uploader
             ref="uploader0"
@@ -45,6 +61,7 @@
             @finish="image => $set(form.files, 0, image.value)"
           />
           <Uploader
+            v-if="!isOrg"
             ref="uploader1"
             :value="files[1]"
             type="file"
@@ -98,26 +115,34 @@ export default {
     return {
       typeMap,
       form: {
+        org_name: '',
+        org_address: '',
         name: '',
         phone: '',
         number: '',
         desc: '',
-        files: [0, 0],
+        files: [undefined, undefined],
       },
       originForm: {},
       loading: false,
       rules: {
-        name: [{ required: true }],
-        number: [{ required: true }],
-        phone: [{ required: true }],
-        desc: [{ required: true }],
-        org_name: [{ required: true }],
-        org_address: [{ required: true }],
-        file: [{ required: true }],
+        name: [{ required: true, message: '必填项' }],
+        number: [{ required: true, message: '必填项' }],
+        phone: [
+          { required: true, message: '必填项', trigger: 'blur' },
+          { type: 'string', pattern: /^1[35789]\d{9}$/, message: '手机号码不合法', trigger: 'blur' },
+        ],
+        desc: [{ required: true, message: '必填项' }],
+        org_name: [{ required: true, message: '必填项' }],
+        org_address: [{ required: true, message: '必填项' }],
+        file: [{ required: true, message: '必填项' }],
       },
     }
   },
   computed: {
+    type () {
+      return this.$route.query.type || 'user'
+    },
     isEdit () {
       return this.$route.params.edit === 'edit'
     },
@@ -126,7 +151,8 @@ export default {
       return certification || {}
     },
     isOrg () {
-      return this.cert.certification_name === 'org'
+      if (this.cert.certification_name) return this.cert.certification_name === 'org'
+      return this.type === 'org'
     },
     labels () {
       return this.isOrg ? orgLabels : userLabels
@@ -148,32 +174,38 @@ export default {
       return !Object.keys(this.changedForm).length
     },
   },
-  fetch ({ store, redirect }) {
+  fetch ({ params, query, store, redirect }) {
     const { logged } = store.state.user
     const cert = logged.certification
-    if (!cert) return redirect('/setting/certificate/apply')
+    if (!cert && params.edit !== 'apply') return redirect('/setting/certificate/apply')
   },
   created () {
-    this.isEdit && this.initData()
+    this.initData()
   },
   methods: {
     async initData () {
-      const form = _.pick(this.cert.data, [...Object.keys(this.labels), 'files'])
+      let form = {}
+      if (this.isEdit) {
+        form = _.pick(this.cert.data, [...Object.keys(this.labels), 'files'])
+        this.form = form
+      } else {
+        this.form.files = new Array(this.isOrg ? 1 : 2).fill()
+        form = this.form
+      }
       this.originForm = _.cloneDeep(form)
-      this.form = form
     },
     async onSubmit () {
       this.loading = true
       await (this.isEdit ? this.onSave() : this.onApply())
-        .finally(() => {
+        .catch(() => {
           this.loading = false
         })
       this.$Message.success('提交成功，请等待审核')
-      this.initData()
-      this.$router.back()
+      await this.$store.dispatch('user/getCurrentUser')
+      this.$router.replace('/setting/certificate')
     },
     onApply () {
-      return this.$axios.$post('/user/certification', this.form)
+      return this.$axios.$post('/user/certification', { ...this.form, type: this.type })
     },
     onSave () {
       return this.$axios.$patch('/user/certification', this.changedForm)
@@ -184,16 +216,48 @@ export default {
 
 <style lang="less" scoped>
 .p-setting-certificate-edit {
+  .nav-wrap {
+    .exact-active {
+      color: @primary-color;
+    }
+  }
+
   .content-wrap {
     padding: 28px 0;
   }
 
   .upload-placeholder {
+    position: relative;
     display: inline-block;
     width: 232px;
     height: 163px;
     margin-right: 8px;
     cursor: pointer;
+
+    figcaption {
+      position: absolute;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      border: 1px dashed @border-color-base;
+      color: @text-info-color;
+      font-size: @font-size-large;
+      font-weight: bold;
+
+      .icon.xl {
+        font-size: 150%;
+      }
+    }
+
+    .image-wrap {
+      position: relative;
+      background-color: transparent;
+      z-index: 1;
+    }
   }
 }
 </style>
