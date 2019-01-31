4e5765276d99a7652198a2327111f1f743df0e12
mutoe <mutoe@foxmail.com>
2019-01-31 12:03:07 +0800

    chore: click outside directive

--- directives/index.js
+++ directives/index.js
@@ -1,9 +1,11 @@
 import empty from './empty'
 import lazyload from './lazyload'
 import ctrlEnter from './ctrlEnter'
+import clickOutside from './clickOutside.js'
 
 export default {
   empty,
   lazyload,
   ctrlEnter,
+  clickOutside,
 }
