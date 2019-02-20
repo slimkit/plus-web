c3067cca08a80ef22a18d67cc525170449d2573a
mutoe <mutoe@foxmail.com>
2019-02-20 16:58:09 +0800

    feat: 注册功能

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -11,19 +11,46 @@
         class="form"
         :label-width="80"
         label-position="left"
-        :model="validator"
-        @submit.native.prevent
+        :model="validateForm"
+        :rules="validator"
+        @submit.native.prevent="onSubmit"
       >
-        <IFormItem prop="account" label="手机号">
-          <IInput v-model="phone" placeholder="输入11位手机号码" />
+        <IFormItem
+          v-if="type === 'phone'"
+          prop="phone"
+          label="手机号"
+        >
+          <IInput
+            v-model="phone"
+            placeholder="输入11位手机号码"
+            :maxlength="11"
+          />
+        </IFormItem>
+
+        <IFormItem
+          v-else
+          prop="email"
+          label="邮箱"
+        >
+          <IInput
+            v-model="email"
+            placeholder="输入邮箱地址"
+            type="email"
+            :maxlength="80"
+          />
         </IFormItem>
 
         <IFormItem prop="code" label="验证码">
-          <IInput v-model="code" placeholder="输入手机验证码">
+          <IInput
+            v-model="code"
+            :placeholder="type === 'phone' ? '输入手机验证码' : '输入邮箱验证码'"
+            :maxlength="6"
+          >
             <IButton
               slot="append"
               type="primary"
               :disabled="codeLimit > 0"
+              :loading="codeLock"
               @click="getCode"
             >
               {{ codeLimit ? `${codeLimit}s 后重试` : '获取验证码' }}
@@ -32,50 +59,152 @@
         </IFormItem>
 
         <IFormItem prop="username" label="设置昵称">
-          <IInput v-model="username" placeholder="2-8个字符" />
+          <IInput
+            v-model="username"
+            placeholder="2-8个字符"
+            :maxlength="8"
+          />
         </IFormItem>
 
         <IFormItem prop="password" label="设置密码">
-          <IInput v-model="password" placeholder="6-15个字符，区分大小写" />
+          <IInput
+            v-model="password"
+            placeholder="6-15个字符，区分大小写"
+            type="password"
+            :maxlength="15"
+          />
         </IFormItem>
 
         <IFormItem prop="confirmPassword" label="确认密码">
-          <IInput v-model="confirmPassword" placeholder="再次输入密码" />
+          <IInput
+            v-model="confirmPassword"
+            placeholder="再次输入密码"
+            type="password"
+            :maxlength="15"
+          />
         </IFormItem>
 
-        <IButton type="primary" html-type="submit">注册</IButton>
+        <IButton
+          type="primary"
+          html-type="submit"
+          :loading="submitLock"
+        >
+          注册
+        </IButton>
       </IForm>
     </main>
   </div>
 </template>
 
 <script>
+import _ from 'lodash'
+
 export default {
   name: 'AuthRegister',
+  middleware: ['forGuest'],
   data () {
     return {
-      codeLimit: 0,
-
       username: '',
       password: '',
       confirmPassword: '',
       phone: '',
       email: '',
       code: '',
+
+      codeLimit: 0,
+      codeLock: false,
+      submitLock: false,
+      validator: {
+        phone: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { type: 'string', pattern: /^1[345789]\d{9}$/, message: '请输入合理的11位手机号', trigger: 'blur' },
+        ],
+        email: [
+          { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+          { type: 'email', message: '请输入正确的邮箱格式', trigger: 'blur' },
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { type: 'string', min: 4, max: 6, message: '验证码格式错误', trigger: 'blur' },
+        ],
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { type: 'string', min: 2, max: 8, message: '用户名长度要求2-8位', trigger: 'blur' },
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { type: 'string', min: 6, max: 15, message: '密码长度为6-15位', trigger: 'blur' },
+        ],
+        confirmPassword: [
+          { required: true, message: '请确认密码', trigger: 'blur' },
+          { type: 'string', min: 6, max: 15, message: '密码长度为6-15位', trigger: 'blur' },
+        ],
+      },
     }
   },
   computed: {
     type () {
       return this.$route.query.type || 'phone'
     },
+    validateForm () {
+      return _.pick(this.$data, Object.keys(this.validator))
+    },
+    form () {
+      const form = {
+        name: this.username,
+        password: this.password,
+        verifiable_type: this.type === 'phone' ? 'sms' : 'mail',
+        verifiable_code: this.code,
+      }
+      if (this.type === 'phone') {
+        form.phone = this.phone
+      } else {
+        form.email = this.email
+      }
+      return form
+    },
   },
   methods: {
-    getCode () {
+    async getCode () {
+      this.$refs.form.validateField(this.type, async error => {
+        if (error) return
+        this.codeLock = true
+        const param = this.type === 'phone' ? { phone: this.phone } : { email: this.email }
+        await this.$axios.$post('/verifycodes/register', param)
+          .finally(() => {
+            this.codeLock = false
+          })
+        this.setCodeLimit()
+      })
+    },
+    setCodeLimit () {
       this.codeLimit = 60
       let timer = setInterval(() => {
         this.codeLimit--
         if (this.codeLimit <= 0) clearInterval(timer)
       }, 1000)
+    },
+    validate () {
+      return new Promise((resolve, reject) => {
+        this.$refs.form.validate(valid => {
+          if (!valid) return reject('[validate] 字段校验失败!')
+          resolve()
+        })
+      })
+    },
+    async onSubmit () {
+      await this.validate()
+      this.submitLock = true
+      const { token } = await this.$axios.$post('/users', this.form)
+        .finally(() => {
+          this.submitLock = false
+        })
+      this.$store.commit('auth/SAVE_TOKEN', token)
+      this.$Message.success('注册成功, 正在跳转...')
+      await this.$store.dispatch('user/getCurrentUser')
+      const { redirect } = this.$route.query
+      if (redirect) this.$router.replace(decodeURIComponent(redirect))
+      else this.$router.back()
     },
   },
 }
