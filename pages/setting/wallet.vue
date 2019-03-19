cf6f1cf9caa1cc5175946f052ce931848f2f9fe8
mutoe <mutoe@foxmail.com>
2019-03-19 20:50:01 +0800

    feat: 我的钱包

---
+++ pages/setting/wallet.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="p-setting-wallet">
+    <nav class="nav-wrap">
+      <nuxt-link :class="{'exact-active': page === 'mine'}" to="/setting/wallet/mine">我的钱包</nuxt-link>
+      <nuxt-link :class="{'exact-active': page === 'detail'}" to="/setting/wallet/detail">交易明细</nuxt-link>
+      <!-- <nuxt-link :class="{'exact-active': page === 'recharge'}" to="/setting/wallet/recharge">充值记录</nuxt-link> -->
+      <nuxt-link :class="{'exact-active': page === 'withdraw'}" to="/setting/wallet/withdraw">提取记录</nuxt-link>
+    </nav>
+
+    <main>
+      <nuxt-child />
+    </main>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SettingWallet',
+  computed: {
+    page () {
+      const match = /^\/setting\/currency\/(\w+)$/.exec(this.$route.path)
+      return match && match[1]
+    },
+  },
+  fetch ({ route, redirect }) {
+    if (route.name === 'setting-wallet') return redirect('/setting/wallet/mine')
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.p-setting-wallet {
+  main {
+    padding: 24px 0;
+  }
+}
+</style>
---
+++ pages/setting/wallet/mine.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="p-setting-wallet-mine">
+    <div class="wallet-wrap">
+      <div>
+        <data class="wallet">{{ (balance || 0).toLocaleString('en-US') }}</data>
+        <p>账户余额（元）</p>
+      </div>
+      <IButton
+        class="recharge-btn"
+        type="primary"
+        size="large"
+      >
+        充值
+      </IButton>
+    </div>
+
+    <hr>
+
+    <footer>
+      <h4>使用规则</h4>
+      <p class="text-pre">{{ config.rule }}</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SettingWalletMine',
+  computed: {
+    config () {
+      return this.boot.wallet
+    },
+    balance () {
+      return this.logged.new_wallet.balance / 100
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.p-setting-wallet-mine {
+  .wallet-wrap {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    padding: 24px 0;
+  }
+
+  .wallet {
+    color: @warning-color;
+    font-size: 42px;
+  }
+
+  .recharge-btn {
+    margin-top: 8px;
+  }
+}
+</style>
